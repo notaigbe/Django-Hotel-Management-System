f4e490534fbfbd3903ec6b46210bb6cebc807071
notaigbe <notaigbe@gmail.com>
2023-06-15 16:41:27 +0100

    switched to canlee-inn.com domain with GoDaddy

--- HMS/settings.py
+++ HMS/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'canlee-inn.onrender.com', 'canleeservicesltd.ng', 'www.canleeservicesltd.ng', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'canlee-inn.onrender.com', 'canleeservicesltd.ng', 'www.canleeservicesltd.ng', 'localhost', 'canlee-inn.com']
 
 # Application definition
 
